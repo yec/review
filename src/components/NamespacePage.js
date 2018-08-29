3afb6d35b943cf50ade8ce4259eb66ed1823cfba
Ye Chuah <yechuah@gmail.com>
2018-08-29 16:10:07 +1000

    fix duplicate key

--- src/components/NamespacePage.js
+++ src/components/NamespacePage.js
@@ -48,7 +48,21 @@ const Pods = ({ pods, namespace, app }) => {
         <strong>{pod.metadata.name} {pod.status.phase}</strong>
         {pod.spec.containers.map(container => {
           const key = pod.metadata.uid + container.name
-          return <pre key={key} style={{fontSize: 12}}>kubectl exec -it {pod.metadata.name} -n {namespace.metadata.name} -c {container.name} /bin/bash</pre>
+          return (
+            <React.Fragment key={key}>
+            <h5>{container.name} container</h5>
+            <div style={{padding: 10, backgroundColor: '#eeeeee'}}>
+            <div>shell</div>
+            <pre style={{fontSize: 12}}>
+              kubectl exec -it {pod.metadata.name} -n {namespace.metadata.name} -c {container.name} /bin/bash
+            </pre>
+            <div>logs</div>
+            <pre style={{fontSize: 12}}>
+              kubectl logs -f {pod.metadata.name} -n {namespace.metadata.name} -c {container.name}
+            </pre>
+            </div>
+            </React.Fragment>
+          );
         })}
       </div>);
     }
@@ -71,23 +85,25 @@ const Ingresses = ({ ingresses, namespace, app }) => {
 }
 
 const AppActions = ({ app, createApp, deleteApp  }) => {
-  return <div>
+  return <React.Fragment>
     <button onClick={e => { createApp(app) }}>create</button>
+    {' '}
     <button onClick={e => { deleteApp(app) }}>delete</button>
-  </div>
+  </React.Fragment>
 };
 
 const AppList = ({ namespace, ingresses, pods }) => {
   return (
     <React.Fragment>
       <h3>Apps</h3>
+      <strong>{namespace.metadata.name}</strong>
+      <div>{namespace.metadata.annotations.branch}</div>
       <ul>
         {Object.keys(apps).map(app => (
           <li key={app}>
-            {app}
-            <Pods       app={app} namespace={namespace} pods={pods} />
+            <strong>{app}</strong> <AppActions app={app} createApp={namespace.createApp} deleteApp={namespace.deleteApp} />
             <Ingresses  app={app} namespace={namespace} ingresses={ingresses} />
-            <AppActions app={app} createApp={namespace.createApp} deleteApp={namespace.deleteApp} />
+            <Pods       app={app} namespace={namespace} pods={pods} />
           </li>
         ))}
       </ul>
@@ -110,7 +126,12 @@ const bindNamespaceActions = (namespace, actionMap) => {
 const NamespacePage = ({ getPods, getIngresses, getNamespace, createNamespace, deleteNamespace, createApp, deleteApp, k8s }) => {
 
   if (!window.hydrate) {
-    window.hydrate = 1;
+    window.hydrate = setInterval(() => {
+      getIngresses();
+      getPods();
+      getNamespace();
+    }, 5000);
+
     getIngresses();
     getPods();
     getNamespace();
@@ -118,14 +139,15 @@ const NamespacePage = ({ getPods, getIngresses, getNamespace, createNamespace, d
 
   return (
     <div>
-      <h1>Review</h1>
+      <h1><Link to="/">Review</Link></h1>
       {/* <h2>Use this site to find review apps for testing.</h2> */}
 
       <CreateNamespaceForm createNamespace={createNamespace} />
 
       <ul>{k8s.namespaces && k8s.namespaces.items.map(item => (
-        <li key={item.metadata.uid}>           {item.status.phase}
-         <Link to={'/app/' + item.metadata.name}>{item.metadata.annotations.branch}</Link>
+        <li key={item.metadata.uid}>
+          {item.status.phase} {' '}
+          <Link to={'/app/' + item.metadata.name}><strong>{item.metadata.annotations.branch}</strong></Link>{' '}
           <button onClick={e => { deleteNamespace(item.metadata.name) } }>delete</button>
           <Route path={'/app/' + item.metadata.name} render={props => <AppList pods={k8s.pods} ingresses={k8s.ingresses} namespace={bindNamespaceActions({...item}, { deleteApp, createApp })} />} />
         </li>))}
@@ -136,10 +158,3 @@ const NamespacePage = ({ getPods, getIngresses, getNamespace, createNamespace, d
 };
 
 export default NamespacePage;
-
-{/* <li>Star Play <button>create</button><button>delete</button> <button>update</button></li>
-<li><Link to="/prop">Star Sydney</Link> <button>create</button><button>delete</button> <button>update</button></li>
-<li><Link to="/prop">Star Goldcoast</Link> <button>create</button><button>delete</button> <button>update</button></li>
-<li><Link to="/prop">Treasury Brisbane</Link> <button>create</button><button>delete</button> <button>update</button></li>
-<li><Link to="/prop">Darling</Link> <button>create</button><button>delete</button> <button>update</button></li>
-<li><Link to="/starpoker">Star Poker</Link> <button>create</button><button>delete</button> <button>update</button></li> */}
